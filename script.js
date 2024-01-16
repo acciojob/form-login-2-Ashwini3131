5506920b13405ca8bedaf35464abd254ed5f3cb7
Ashwini3131 <93239185+Ashwini3131@users.noreply.github.com>
2024-01-16 17:08:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function getFormvalue() {
+    //Write your code here
+	let first = document.getElementById("fname").value;
+	let last = document.getElementById("lname").value;
+    let ph=document.getElementById("phno").value;
+    let email=document.getElementById("email").value;
+	alert('First Name:'+first+'\nLast Name:'+last+'\nPhone Number:'+ph+'\nEmail ID:'+email);
+}
